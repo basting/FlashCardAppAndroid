e8b8cc045a2ea7ef819bbc06624dae541bda939c
basting <bastin.gomez@gmail.com>
2015-10-04 21:28:32 -0400

    more changes

--- app/src/main/java/com/bighi/flashcard/MainActivity.java
+++ app/src/main/java/com/bighi/flashcard/MainActivity.java
@@ -5,11 +5,16 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.firebase.client.Firebase;
+
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Firebase.setAndroidContext(this);
+
         setContentView(R.layout.activity_main);
     }
 
