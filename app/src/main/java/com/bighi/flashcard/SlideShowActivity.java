785404871724e688fdb7cc5835cf6c97bb78ced5
basting <bastin.gomez@gmail.com>
2015-10-15 22:28:57 -0400

    slideshow changes

---
+++ app/src/main/java/com/bighi/flashcard/SlideShowActivity.java
@@ -0,0 +1,14 @@
+package com.bighi.flashcard;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class SlideShowActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_slide_show);
+    }
+
+}
