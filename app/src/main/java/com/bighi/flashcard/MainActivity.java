ec78416f24827ee1f703d2baf6b394361212f81d
basting <bastin.gomez@gmail.com>
2015-10-17 14:37:13 -0400

    slideshow changes

--- app/src/main/java/com/bighi/flashcard/MainActivity.java
+++ app/src/main/java/com/bighi/flashcard/MainActivity.java
@@ -153,7 +153,6 @@ public class MainActivity extends Activity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             Intent intent = new Intent();
             intent.setClass(MainActivity.this, SettingsActivity.class);
